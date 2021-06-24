cb01532d1d5dca9d373dcb69d03f8adbd5229a62
Benjamin israel Destefanis <56180439+B4NJY@users.noreply.github.com>
2021-06-24 06:17:16 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,93 @@
+const botonNumeros = document.getElementsByName('data-number');
+const botonOpera = document.getElementsByName("data-opera");
+const botonIgual = document.getElementsByName("data-igual")[0];
+const botonPunto = document.getElementsByName("data-punto")[0];
+const botonBorrar = document.getElementsByName("data-borrar")[0];
+
+var result = document.getElementById('result');
+var opeActual = '';
+var opeAnterior = '';
+var operacion = undefined;
+
+
+botonNumeros.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        agregarNumero(boton.innerText);
+    })
+})
+
+botonOpera.forEach(function(boton){
+    boton.addEventListener('click', function(){
+       selectOperacion(boton.innerText);
+    })
+})
+
+botonIgual.addEventListener('click', function(){
+    calcular();
+    actualizarDisplay();
+})
+
+botonPunto.addEventListener('click', function(){
+    selectPunto(botonPunto.innerText);
+})
+
+botonBorrar.addEventListener('click', function(){
+    clear();
+    actualizarDisplay();
+})
+
+
+
+function agregarNumero(num){
+    opeActual = opeActual.toString() + num;
+    actualizarDisplay();
+}
+
+function selectOperacion(op){
+    if (opeActual === '') return;
+    if (opeAnterior !== ''){
+        calcular()
+    }
+    operacion = op.toString();
+    opeAnterior = opeActual;
+    opeActual = '';
+}
+
+function calcular(){
+    var calculo;
+    const anterior = parseFloat(opeAnterior);
+    const actual = parseFloat(opeActual);
+    if (isNaN(anterior) || isNaN(actual)) return;
+    switch(operacion){
+        case '+':
+            calculo = anterior + actual ;
+            break;
+        case '-':
+            calculo = anterior - actual ;
+            break;
+        case 'x':
+            calculo = anterior * actual ;
+            break;
+        case '/':
+            calculo = anterior / actual ;
+            break;
+        default:
+            return;        
+    }
+    opeActual = calculo;
+    operacion = undefined;
+    opeAnterior = '';
+}
+
+function clear(){
+    opeActual = '';
+    opeAnterior = '';
+    operacion = undefined;
+}
+
+function actualizarDisplay(){
+     result.value = opeActual;
+    
+}
+
+
